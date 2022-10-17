301dca83c6a15469a141e0f4e6d01bffe7b1cc34
guatamsood15 <gauty22@gmail.com>
2022-10-17 22:45:37 +0530

    Adding new code for url correction

--- api/views.py
+++ api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Note
+from .serializers import NoteSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
@@ -46,4 +48,12 @@ def getRoutes(request):
 @api_view(['GET'])
 def getNotes(request):
     notes = Note.objects.all()
-    return Response('NOTES')
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
